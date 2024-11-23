ecf40f6b96d587598e978a26d38a59f5e4fbedd3
Jeremy Mowery <jermowery@gmail.com>
2024-11-23 13:29:19 -0800

    Add back ci workflow

--- src/register.js
+++ src/register.js
@@ -34,7 +34,7 @@ const response = await fetch(url, {
     Authorization: `Bot ${token}`,
   },
   method: 'PUT',
-  body: JSON.stringify([AWW_COMMAND, INVITE_COMMAND]),
+  body: JSON.stringify([]),
 });
 
 if (response.ok) {
